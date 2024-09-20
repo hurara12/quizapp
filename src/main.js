b52bbc39f8cc376ee5686cb3b283a7b14b122b48
hurara12 <hurara.waseem@gmail.com>
2024-09-20 14:48:15 +0500

    new repo

--- src/main.js
+++ src/main.js
@@ -2,5 +2,18 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 
-createApp(App).use(store).use(router).mount('#app')
+
+library.add(fas);
+
+const app = createApp(App);
+
+app.component('font-awesome-icon', FontAwesomeIcon);
+app.use(store);
+app.use(router);
+app.mount('#app');
